e4a48acf0f8cca1b07310b0a3c5beb59cf005c1e
Ilia Yakubovsky <silkbottle@gmail.com>
2017-04-14 15:47:24 +0300

    update dataset

--- code/dataset.py
+++ code/dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import multiprocessing
 import re
 import pickle
+import progressbar
 from tokenizer import MosesTokenizer, MosesDetokenizer
 
 def encode_field(*args):
@@ -51,9 +52,18 @@ def unknown(token):
     else:
         return '<unk>'
     
+def multinomial(probs):
+    x = np.random.sample()
+    s = probs[0]
+    i = 0
+    while x >= s:
+        i += 1
+        s += probs[i]
+    return i
+    
 
 class Dataset(object):
-    def __init__(self, english, french, num_words=200000, buckets=[20, 40, 200], nthreads=4):
+    def __init__(self, english, french, num_words=200000, buckets=[20, 40, 200], nthreads=4, ratio=0.1):
         self.tokenizer = {}
         self.detokenizer = {}
         self.source = {}
@@ -82,6 +92,12 @@ class Dataset(object):
             self.__build_vocab(lang)
         
         self.__init_tensors()
+        
+        for lang in ['en', 'fr']:
+            self.__fill_tensors(lang)
+            
+        self.test_train_split(ratio=ratio)
+        
         
     
     def __build_vocab(self, lang):
@@ -144,7 +160,33 @@ class Dataset(object):
                      'length': np.zeros(lengths[i], dtype=np.int)}
                 )
                     
-    def fill_tensors(self, lang):
+    def __fill_tensors(self, lang):
+        
+        def bucket_id(length):
+            bucket = 0
+            while bucket < len(self.buckets_sizes) - 1 and self.buckets_sizes[bucket] < length:
+                bucket += 1
+            return bucket
+        
+        def fill_decoder(sentence, bucket, pos):
+            end = min(len(sentence), self.buckets_sizes[bucket]) + 1
+            self.decoder[lang][bucket]['length'][pos] = end
+            self.decoder[lang][bucket]['x'][pos][0] = self.vocab[lang]['<s>']           
+            for i in range(1, end):
+                self.decoder[lang][bucket]['x'][pos][i] = sentence[i - 1]
+                self.decoder[lang][bucket]['y'][pos][i - 1] = sentence[i - 1]
+            if end == len(sentence) + 1:
+                self.decoder[lang][bucket]['y'][pos][end - 1] = self.vocab[lang]['<\s>']
+            else:
+                self.decoder[lang][bucket]['y'][pos][end - 1] = sentence[end - 1]
+        
+        def fill_encoder(sentence, bucket, pos):
+            start = max(self.buckets_sizes[bucket] - len(sentence), 0)
+            for i in range(start, self.buckets_sizes[bucket]): 
+                self.encoder[lang][bucket]['x'][pos][i] = sentence[i - start]
+                self.encoder[lang][bucket]['length'][pos] = self.buckets_sizes[bucket] - start
+        
+        
         with open(self.source[lang], 'r') as f:
             buffer = []
             counters = np.zeros(len(self.buckets_sizes), dtype=np.int)
@@ -153,42 +195,56 @@ class Dataset(object):
                 buffer.append(line)
                 if len(buffer) >= self.per_thread * self.nthreads:
                     results = tokenize(self.tokenizer[lang], buffer, self.per_thread)
-                    for r in results:
-                        r_ = []
-                        for w in r:
-                            r_.append(self.vocab[lang].get(w, self.vocab[lang][unknown(w)]))
-                        bucket_id = 0
-                        current_len = max(self.lengths['en'][index], self.lengths['fr'][index])
-                        while bucket_id < len(self.buckets_sizes) - 1 and self.buckets_sizes[bucket_id] < current_len:
-                            bucket_id += 1
-                            
-                        #fill encoder
-                        start = max(self.buckets_sizes[bucket_id] - len(r_), 0)
-                        for i in range(start, self.buckets_sizes[bucket_id]): 
-                            self.encoder[lang][bucket_id]['x'][counters[bucket_id]][i] = r_[i - start]
-                        self.encoder[lang][bucket_id]['length'][counters[bucket_id]] =\
-                            self.buckets_sizes[bucket_id] - start
-                        
-                        #fill decoder
-                        end = min(len(r_) + 1, self.buckets_sizes[bucket_id] + 1)
-                        self.decoder[lang][bucket_id]['length'][counters[bucket_id]] = end
-                        r_ = [self.vocab[lang]['<s>']] + r_ + [self.vocab[lang]['<\s>']]
-                        
-                        for i in range(end):
-                            self.decoder[lang][bucket_id]['x'][counters[bucket_id]][i] = r_[i]
-                            self.decoder[lang][bucket_id]['y'][counters[bucket_id]][i] = r_[i + 1]
-                            
-                        counters[bucket_id] += 1
+                    for res in results:
+                        sentence = [self.vocab[lang].get(w, self.vocab[lang][unknown(w)]) for w in res]
+                        bucket = bucket_id(max(self.lengths['en'][index], self.lengths['fr'][index])) 
+                        fill_encoder(sentence, bucket, counters[bucket])
+                        fill_decoder(sentence, bucket, counters[bucket])
+                        counters[bucket] += 1
                         index += 1
                     buffer = []
                             
             if len(buffer) > 0:
                 results = tokenize(self.tokenizer[lang], buffer, self.per_thread)
+                for res in results:
+                    sentence = [self.vocab[lang].get(w, self.vocab[lang][unknown(w)]) for w in res]
+                    bucket = bucket_id(max(self.lengths['en'][index], self.lengths['fr'][index])) 
+                    fill_encoder(sentence, bucket, counters[bucket])
+                    fill_decoder(sentence, bucket, counters[bucket])
+                    counters[bucket] += 1
+                    index += 1
                         
     
-    def test_train_split(self, ratio=0.01, random_seed=42):
-        np.random.seed(random_seed)
-        pass
+    def test_train_split(self, ratio=0.01, seed=42):
+        np.random.seed(seed)
+        
+        self.random_seed = seed
+        self.ratio = ratio
+        
+        self.train = {}
+        self.test = {}
+        for tag in ['encoder', 'decoder']:
+            self.train[tag] = {}
+            self.test[tag] = {}
+            for lang in ['en', 'fr']:
+                self.train[tag][lang] = []
+                self.test[tag][lang] = []
+                
+        for i in range(len(self.buckets_sizes)):
+            size = self.encoder['en'][i]['x'].shape[0]
+            test_size = int(ratio * size)
+            perm = np.random.permutation(size)
+            for lang in ['en', 'fr']:
+                self.train['encoder'][lang].append({})
+                self.test['encoder'][lang].append({})
+                for tag in ['x', 'length']:
+                    self.train['encoder'][lang][i][tag] = self.encoder[lang][i][tag][perm[test_size:]]
+                    self.test['encoder'][lang][i][tag] = self.encoder[lang][i][tag][perm[:test_size]]
+                self.train['decoder'][lang].append({})
+                self.test['decoder'][lang].append({})
+                for tag in ['x', 'y', 'length']:
+                    self.train['decoder'][lang][i][tag] = self.decoder[lang][i][tag][perm[test_size:]]
+                    self.test['decoder'][lang][i][tag] = self.decoder[lang][i][tag][perm[:test_size]]
     
     def encode(self, string, lang='en'):
         tokens = self.tokenizer[lang].tokenize(string)
@@ -205,10 +261,58 @@ class Dataset(object):
             result.append(self.ivocab[lang][t[i]])
         return self.detokenizer[lang].detokenize(result)
     
+    def iterate_minibatches(self, batch_size, mode='train', show=True):
+        if mode == 'train':
+            data = self.train
+        if mode == 'test':
+            data = self.test
+            
+        perms = []
+        idxs = []
+        sizes = []
+        fullsize = 0
+        for i in range(len(self.buckets_sizes)):
+            sizes.append(data['encoder']['en'][i]['x'].shape[0])
+            perms.append(np.random.permutation(sizes[i]))
+            fullsize += data['encoder']['en'][i]['x'].size
+            
+        sizes = np.array(sizes)
+        probs = sizes / sizes.sum()
+        idxs = np.zeros(sizes.size, dtype=np.int)
+        mask = np.ones(sizes.size, dtype=np.bool)
+        
+        if show:
+            bar = progressbar.ProgressBar(max_value=fullsize)
+            progress = 0
+        finish = False
+        while not finish:
+            bucket = multinomial(probs)
+            start = idxs[bucket]
+            end = idxs[bucket] + batch_size
+            if end > sizes[bucket]:
+                mask[bucket] = 0
+                if mask.sum() == 0:
+                    finish = True
+                else:
+                    probs = np.multiply(probs, mask)
+                    probs /= probs.sum()
+                end = sizes[bucket]
+            idxs[bucket] = end
+            
+            if show:
+                progress += data['encoder']['en'][bucket]['x'][perms[bucket][start:end]].size
+                print(progress)
+                bar.update(progress)
+                
+            yield data['encoder']['en'][bucket]['x'][perms[bucket][start:end]], \
+                data['encoder']['en'][bucket]['length'][perms[bucket][start:end]], \
+                data['encoder']['fr'][bucket]['x'][perms[bucket][start:end]], \
+                data['encoder']['fr'][bucket]['length'][perms[bucket][start:end]]
+
     def save(self, filename):
         fields = {}
         for k, v in self.__dict__.items():
-            if not k in ['encoder', 'decoder', 'tokenizer', 'detokenizer']:
+            if not k in ['encoder', 'decoder', 'tokenizer', 'detokenizer', 'train', 'test']:
                 fields[k] = v
         
         data = {}
@@ -228,10 +332,10 @@ class Dataset(object):
     def load(filename):
         
         with open(filename + '.pcl', 'rb') as f:
-            d_ = pickle.load(f)
+            d = pickle.load(f)
             
         dataset = Dataset.__new__(Dataset)
-        dataset.__dict__ = d_
+        dataset.__dict__ = d
         for tag in ['encoder', 'decoder']:
             dataset.__dict__[tag] = {}
             for lang in ['en', 'fr']:
@@ -249,6 +353,7 @@ class Dataset(object):
         for lang in ['en', 'fr']:
             dataset.tokenizer[lang] = MosesTokenizer(lang=lang)
             dataset.detokenizer[lang] = MosesDetokenizer(lang=lang)
-                
-                    
+            
+        if 'random_seed' in d and 'ratio' in d:
+            dataset.test_train_split(ratio=d['ratio'], seed=d['random_seed'])       
         return dataset
